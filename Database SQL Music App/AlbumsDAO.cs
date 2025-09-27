63183b62baa1b2c7a4149c6c422a619a0089cc38
Christin912 <92063837+Christin912@users.noreply.github.com>
2025-09-27 03:05:36 -0600

    further updated DAO, still need to connect

---
+++ Database SQL Music App/AlbumsDAO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database_SQL_Music_App
+{
+    internal class AlbumsDAO
+    {
+       string connectionString = "datasource=localhost;port "  
+
+    }
+}
--- Database SQL Music App/Form1.cs
+++ Database SQL Music App/Form1.cs
@@ -2,6 +2,8 @@ namespace Database_SQL_Music_App
 {
     public partial class Form1 : Form
     {
+        BindingSource albumBindingSource = new BindingSource();
+
         public Form1()
         {
             InitializeComponent();
@@ -21,5 +23,41 @@ namespace Database_SQL_Music_App
         {
 
         }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            AlbumsDAO albumsDAO = new AlbumsDAO();
+            Album a1 = new Album
+
+            {
+                ID = 1,
+                AlbumName = "The Dark Side of the Moon",
+                ArtistName = "Pink Floyd",
+                ReleaseYear = 1973,
+                ImageURL = "https://example.com/darkside.jpg",
+                description = "A landmark album in rock history."
+            };
+            Album a2 = new Album
+            {
+                ID = 1,
+                AlbumName = "The darker Side of the Moon",
+                ArtistName = "Pink man",
+                ReleaseYear = 1973,
+                ImageURL = "https://example.com/darkside.jpg",
+                description = "A landmark album in rock history."
+
+
+            };
+            albumsDAO.albums.Add(a1);
+            albumsDAO.albums.Add(a2);
+
+            //connect the list to the grid view
+            albumBindingSource.DataSource = albumsDAO.albums;
+
+            dataGridView1.DataSource = albumBindingSource;
+
+
+        }
     }
 }
+    
