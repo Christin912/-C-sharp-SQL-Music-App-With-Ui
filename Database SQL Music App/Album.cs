dc10765c0e296587e0c4016df7e12fe86df27289
Christin912 <92063837+Christin912@users.noreply.github.com>
2025-09-27 02:38:47 -0600

    Add project files.

---
+++ Database SQL Music App/Album.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database_SQL_Music_App
+{
+    internal class Album
+    {
+        public int ID { get; set; }
+        public string AlbumName { get; set; }
+        public string ArtistName { get; set; }
+        public int ReleaseYear { get; set; }
+        public string ImageURL { get; set; }
+        public string description { get; set; }
+
+        // coming soon: public List<Song> Songs { get; set; } songs in album
+    }
+}
